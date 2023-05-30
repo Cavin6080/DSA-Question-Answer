e3e63a9eb7bca2c97c9625a37866ac2e2739fc83
cavin6080 <cavinmacwan1@gmail.com>
2023-05-30 08:54:05 +0530

    [Array] solved array question of permutation 🚀

---
+++ arrays/ArrayFromPermutation.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Arrays;
+
+/*
+Build Array from Permutation
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length 
+and return it.
+
+Link: https://leetcode.com/problems/build-array-from-permutation/description/
+ */
+
+public class ArrayFromPermutation {
+
+    static int[] buildArray(int[] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 2, 1, 5, 3, 4 };
+        int[] ans = buildArray(nums);
+
+        System.out.println(Arrays.toString(ans));
+    }
+
+}
